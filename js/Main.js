fb553f192431846cb0e913f5550c222867bf2818
ahmed500fa <ahmed5722144@gmail.com>
2024-11-06 11:35:47 +0200

    initial commit

---
+++ js/Main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.type', {
+    strings: [
+        'Developer'
+        , 'Designer'
+        ,'Photographer'
+    ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+  });
